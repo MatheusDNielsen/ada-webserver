d4ac2339a89538ff258717061f4cd1d08fe4f667
Matheus Nielsen <matheusdnielsen@gmail.com>
2025-12-14 22:53:15 -0400

    Check if provided folder is folder

--- src/hello_world.adb
+++ src/hello_world.adb
@@ -1,3 +1,6 @@
+with Ada.Directories;
+with Ada.Strings.Fixed;
+with Ada.Strings.Unbounded;
 with Ada.Text_IO;
 with Ada.Command_Line;
 with GNAT.Sockets;
@@ -6,17 +9,32 @@ with Server;
 procedure Hello_World is
   package T_IO renames Ada.Text_IO;
   package C_L renames Ada.Command_Line;
+  package U renames Ada.Strings.Unbounded;
+  package D renames Ada.Directories;
 
   procedure Get_Parameters
-   (Port_Number : out Integer; Folder_To_Serve : out String) is
+   (Port_Number : out Integer; Folder_To_Serve : out U.Unbounded_String) is
   begin
     Port_Number := Integer'Value (C_L.Argument (1));
-    Folder_To_Serve := C_L.Argument (2);
+    Folder_To_Serve := U.To_Unbounded_String (C_L.Argument (2));
   end Get_Parameters;
 
+  procedure Get_Folder_To_Serve (Folder_To_Serve : in out U.Unbounded_String)
+  is
+    package S_F renames Ada.Strings.Fixed;
+    use type Ada.Directories.File_Kind;
+
+  begin
+    if (D.Kind (U.To_String (Folder_To_Serve)) = D.Directory) then
+      T_IO.Put_Line ("Is Dir");
+    else
+      raise D.Name_Error;
+
+    end if;
+  end Get_Folder_To_Serve;
 
   Port_Number     : Integer := 8000;
-  Folder_To_Serve : String := ".";
+  Folder_To_Serve : U.Unbounded_String := U.To_Unbounded_String (".");
   Server_Socket   : GNAT.Sockets.Socket_Type;
 
 begin
@@ -31,6 +49,15 @@ begin
   end if;
 
   Get_Parameters (Port_Number, Folder_To_Serve);
+
+  begin
+    Get_Folder_To_Serve (Folder_To_Serve);
+  exception
+    when E : D.Name_Error =>
+      T_IO.Put_Line (T_IO.Standard_Error, "Error: Path is not a folder");
+      C_L.Set_Exit_Status (1);
+      return;
+  end;
 
   Server_Socket := Server.Open_TCP_Connection (Port_Number);
 
